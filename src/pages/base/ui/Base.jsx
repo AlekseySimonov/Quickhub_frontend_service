eb55d1573a3f7253d6bab8ebd81314fd0cd596fe
AlekseySimonov <aleksey.simonov04@mail.ru>
2024-11-26 11:46:53 +0300

    Deleted Loader from Base

--- src/pages/base/ui/Base.jsx
+++ src/pages/base/ui/Base.jsx
@@ -2,34 +2,18 @@ import { Outlet } from "react-router-dom";
 import {Header} from "../../../widgets/header/index";
 import {Menu} from "../../../widgets/menu/index";
 import styles from './styles.module.css';
-import {useEffect, useState } from "react";
+import { useState } from "react";
 import { baseTitles } from "../../routing";
-import { useSelector } from "react-redux";
-import { Loader } from "../../../shared/ui/components";
 import { usePageTitle } from "../../../shared/hooks/index";
 
 export const Base = () => {
 
-    const {status} = useSelector(state => state.company)
     usePageTitle(baseTitles)
 
     const [isActive, setIsActive] = useState(false)
     const menu = ()=>{
         setIsActive(!isActive)
     }
-
-    const [showLoader, setShowLoader] = useState(false)
-    useEffect(() => {
-        if (status === 'loading') {
-            const timer = setTimeout(() => {
-                setShowLoader(true);
-            }, 200)
-
-            return () => clearTimeout(timer)
-        } else {
-            setShowLoader(false)
-        }
-    }, [status]);
 
     return (
         <div className = {styles.container}
@@ -43,8 +27,7 @@ export const Base = () => {
         <div className= {styles.menu}>
             <Menu isActive = {isActive} />
         </div>
-        <div className={`${styles.content} ${status === 'loading' ? styles.loading : ''}`}>
-            {showLoader && (<Loader style = {styles.loader}/>)}
+        <div className={styles.content}>
             <Outlet/>
         </div>
         </div>
--- src/widgets/companies/ui/Companies.jsx
+++ src/widgets/companies/ui/Companies.jsx
@@ -4,10 +4,12 @@ import styles from './styles.module.css'
 import { useEffect } from "react"
 import { useDispatch, useSelector} from "react-redux"
 import { getCompaniesAPI, setCompanyID, checkCompanyID} from "../../../app/store/slices/companySlice"
+import { Loader } from "../../../shared/ui/components"
 
 export const Companies = () =>{
     const dispatch = useDispatch()
     const {companyID, status} = useSelector(state => state.company)
+
     useEffect(() => {
         dispatch(getCompaniesAPI())
     }, [dispatch])
@@ -24,8 +26,9 @@ export const Companies = () =>{
         <>
         <div className={styles.content}>
             <CompaniesHeader/>
-            <div className={styles.main}>
-                <Outlet />
+            <div className={`${styles.main} ${status === 'loading' ? styles.loading : ''}`}>
+                {status === 'loading' && (<Loader style = {styles.loader}/>)}
+                <Outlet/>
             </div>
         </div>
         </>
